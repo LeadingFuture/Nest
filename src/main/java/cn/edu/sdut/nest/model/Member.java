eb48b97ea4901c1a6815407c8c13df687a18cf55
momocmg <1026610078@qq.com>
2014-09-30 09:55:10 +0800

    add junit test

--- src/main/java/cn/edu/sdut/nest/model/Member.java
+++ src/main/java/cn/edu/sdut/nest/model/Member.java
@@ -1,5 +1,6 @@
 package cn.edu.sdut.nest.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -13,6 +14,7 @@ public class Member {
     private String lastName;
     private String firstName;
 
+   // @Column(length=2)
     public String getFirstName() {
         return firstName;
     }
